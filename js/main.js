a29b0c8af3f8ca3ba843837ada0d4a047f2d7ef6
Dima Kovalenko <dkovalen@e1r9p4.unit.ua>
2017-11-25 22:16:57 +0200

    v3

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ var maxSize = 6;
 
 function setPuzzle() {
 	var size = $('#size').val();
+
+	if ()
 	for (var i = 1; i < Math.pow(size, 2); i++) {
 		$('#puzzle').append("<div class='tile' id='tile- " + i + "'>" + i + "</div>");
 	}
-	$('#puzzle').append("<div class='empty' id='tile-0'></div>");
+	$('#puzzle').append("<div class='tile' id='tile-0'>-</div>");
+	$('#puzzle').css('height', $('#puzzle').css('width'));
 }
 
 function setSize() {
