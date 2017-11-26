a762fd764f1baf5239e97e9ce50e9d5607d1f7ee
Dima Kovalenko <dkovalen@e1r11p3.unit.ua>
2017-11-26 14:55:54 +0200

    av

--- router.py
+++ router.py
@@ -11,8 +11,9 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def hello_world():
-	url_for('static', filename='style.css')
+def main():
+	url_for('web', filename='style.css')
+	url_for('web/js', filename='style.css')
 	return jsonify(data)
 
 @app.route('/tst', methods=['POST'])
