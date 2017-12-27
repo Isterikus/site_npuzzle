a1756e2aadb1b1131eed94248e5764b1067a47a4
Isterikus <kovalenko27071996@gmail.com>
2017-12-27 19:14:55 +0200

    srav

--- router.py
+++ router.py
@@ -32,5 +32,37 @@ def solve():
 	# print("TIME = ", rez[''])
 	return json.dumps(rez)
 
+@app.route('/parse_file', methods=['POST'])
+def parse_file():
+	file = request.values['file']
+	initial_field = []
+	i = 0
+	n = 0
+	for line in file.split('\n'):
+		j = 0
+		if line == "":
+			continue
+		# line = line.strip()
+		print("[", line, "]")
+		if line[0] != '#' and not line[0].isdigit():
+			return json.dumps({'error': 'Bad file'})
+		elif line[0] == '#':
+			pass
+		elif n == 0:
+			n = int(line)
+			if n < 1:
+				return json.dumps({'error': 'Bad file'})
+			field_size = pow(n, 2)
+			initial_field = [0 for s in range(field_size)]
+		else:
+			line = [s for s in line.split(' ') if s != '']
+			for j in range(n):
+				try:
+					initial_field[i * n + j] = int(line[j])
+				except:
+					return json.dumps({'error': 'Bad file'})
+			i += 1
+	return json.dumps({'size': n, 'field': initial_field})
+
 if __name__ == '__main__':
 	app.run(debug=True)
